1d69972c56bda4362648e0770e1f287c0d7ea60d
Vladimir Valouch <vladimir.valouch@concur.com>
2018-12-12 16:33:22 +0100

    version for lesson start

--- ImageTricks/app/src/main/java/com/concur/test/imagetricks/ImageLoader.kt
+++ ImageTricks/app/src/main/java/com/concur/test/imagetricks/ImageLoader.kt
@@ -25,81 +25,32 @@ class ImageLoader(private val context: Context) {
         "https://themostbeautifulplacesineurope.files.wordpress.com/2013/06/dsc_0388.jpg",
         "https://mikarchitect.files.wordpress.com/2010/07/cfw-chr-high-resolution.jpg",
         "https://phxarch.files.wordpress.com/2014/04/rappaport.jpg",
-        "https://cdn.history.com/sites/2/2015/09/iStock_000038100826_Large.jpg",
-        "https://rwethereyetmom.com/wp-content/uploads/2016/04/DSC_0107_2.jpg",
+        "https://cdn.history.com/sites/2/2015/09/iStock_000038100826_Large.jpg"
+        /*
+        ,"https://rwethereyetmom.com/wp-content/uploads/2016/04/DSC_0107_2.jpg",
         "https://www.worldfortravel.com/wp-content/uploads/2015/10/Arches-National-Park-Utah-US.jpg",
         "https://www.worldfortravel.com/wp-content/uploads/2015/04/Yellowstone-National-Park-US.jpg",
         "https://anotherheader.files.wordpress.com/2010/11/pano_02301.jpg",
         "https://i1.wp.com/switchbackkids.com/wp-content/uploads/2016/02/IMG_6445.jpg?fit=3264%2C2448",
         "https://upload.wikimedia.org/wikipedia/commons/e/ee/Tasikmalaya_Regency_hilly_view_2_-_high_res.jpg",
-        "https://car-images.bauersecure.com/pagefiles/73784/bmw_x3_2017_11.jpg"
+        "https://car-images.bauersecure.com/pagefiles/73784/bmw_x3_2017_11.jpg",
+        "https://images.freeimages.com/images/large-previews/5c6/sunset-jungle-1383333.jpg",
+        "https://images.freeimages.com/images/large-previews/e4a/surf-3-1378236.jpg",
+        "https://images.freeimages.com/images/large-previews/e82/beach-surfing-buggy-s-1462290.jpg",
+        "https://images.freeimages.com/images/large-previews/3ff/pray-for-wave-1469024.jpg",
+        "https://images.freeimages.com/images/large-previews/43a/positure-ii-1170594.jpg",
+        "https://images.freeimages.com/images/large-previews/525/pen-1311782.jpg",
+        "https://images.freeimages.com/images/large-previews/209/pen-drive-1239890.jpg"
+        */
     )
 
     suspend fun getBitmapBadWay(position: Int): Bitmap? {
         var bitmap: Bitmap? = null
         withContext(Dispatchers.IO) {
-            val tempFile = File(context.cacheDir,"$position-${dataSet[position].hashCode()}.jpg")
-            /*
-            if (tempFile.exists()) {
-                val options = BitmapFactory.Options()
-                options.inJustDecodeBounds = true
-                BitmapFactory.decodeFile(tempFile.absolutePath, options)
-
-                val imageHeight: Int = options.outHeight
-                val imageWidth: Int = options.outWidth
-
-                val scale = Math.max(imageHeight, imageWidth) / 200
-
-                options.inJustDecodeBounds = false
-                options.inSampleSize = scale
-
-                bitmap = BitmapFactory.decodeFile(tempFile.absolutePath, options)
-
-            } else {*/
-                URL(dataSet[position]).openStream().use { input ->
-
-                    FileOutputStream(tempFile).buffered().use {
-                        it.write(input.readBytes())
-                    }
-                    val options = BitmapFactory.Options()
-                    options.inJustDecodeBounds = true
-                    BitmapFactory.decodeFile(tempFile.absolutePath, options)
-
-                    val imageHeight: Int = options.outHeight
-                    val imageWidth: Int = options.outWidth
-
-                    val scale = 1 //Math.max(imageHeight, imageWidth) / 200
-
-                    options.inJustDecodeBounds = false
-                    options.inSampleSize = scale
-
-                    bitmap = BitmapFactory.decodeFile(tempFile.absolutePath, options)
-                //}
+            URL(dataSet[position]).openStream().use { input ->
+                bitmap = BitmapFactory.decodeStream(input)
             }
         }
         return bitmap
-    }
-
-    fun setBitmapPicasso(position: Int, imageView: ImageView) {
-        Picasso.get()
-            .load(dataSet[position])
-            .placeholder(android.R.drawable.alert_dark_frame)
-            //.fit()
-            .resize(200, 200).centerInside()
-            .transform(object : Transformation {
-                override fun key(): String {
-                    return "myCustomTransformation"
-                }
-
-                override fun transform(source: Bitmap?): Bitmap {
-                    val matrix = Matrix()
-                    matrix.postRotate(90f)
-                    matrix.postScale(0.1f, 0.1f)
-                    val result = Bitmap.createBitmap(source, 0, 0, source!!.width, source!!.height, matrix, true)
-                    source.recycle()
-                    return result
-                }
-            })
-            .into(imageView)
     }
 }
